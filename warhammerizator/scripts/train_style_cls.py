442d68e6ace9dd03c727b0154f1e4c8516097fe5
Stepan Kudin <kudin.stepan@yandex.ru>
2025-05-26 23:18:05 +0300

    Read settings.

--- warhammerizator/scripts/train_style_cls.py
+++ warhammerizator/scripts/train_style_cls.py
@@ -1,5 +1,8 @@
 import argparse
 from pathlib import Path
+from typing import Dict
+
+import yaml
 
 
 def parse_command_prompt() -> argparse.Namespace:
@@ -11,7 +14,16 @@ def parse_command_prompt() -> argparse.Namespace:
 
 def main():
     args = parse_command_prompt()
+
+    settings = read_settings(args.settings)
+
     print("Hello world!")
+
+
+def read_settings(filename: Path) -> Dict:
+    with open(filename, "r") as fp:
+        return yaml.load(stream=fp, Loader=yaml.FullLoader)
+
 
 
 if __name__ == "__main__":
