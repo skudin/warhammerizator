1ad73859404a8b4cd3dc4a8ce192bf7d2eae005d
Stepan Kudin <kudin.stepan@yandex.ru>
2025-05-26 23:01:33 +0300

    Add command prompt args.

--- warhammerizator/scripts/train_style_cls.py
+++ warhammerizator/scripts/train_style_cls.py
@@ -1,4 +1,16 @@
+import argparse
+from pathlib import Path
+
+
+def parse_command_prompt() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("settings", type=Path, help="path to training settings")
+
+    return parser.parse_args()
+
+
 def main():
+    args = parse_command_prompt()
     print("Hello world!")
 
 
