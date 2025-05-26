23884afffc21402841ca19f4c085a2099ef91a2a
Stepan Kudin <kudin.stepan@yandex.ru>
2025-05-26 23:37:40 +0300

    Prepare dataset.

--- warhammerizator/scripts/train_style_cls.py
+++ warhammerizator/scripts/train_style_cls.py
@@ -1,8 +1,11 @@
 import argparse
 from pathlib import Path
-from typing import Dict
+from typing import Dict, Tuple
 
 import yaml
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+from datasets import Dataset
 
 
 def parse_command_prompt() -> argparse.Namespace:
@@ -17,6 +20,8 @@ def main():
 
     settings = read_settings(args.settings)
 
+    train, val, test, le = prepare_dataset(settings["dataset"])
+
     print("Hello world!")
 
 
@@ -24,6 +29,20 @@ def read_settings(filename: Path) -> Dict:
     with open(filename, "r") as fp:
         return yaml.load(stream=fp, Loader=yaml.FullLoader)
 
+
+def prepare_dataset(dataset_paths: Dict[str, str]) -> Tuple[Dataset, Dataset, Dataset, LabelEncoder]:
+    train_df = pd.read_csv(dataset_paths["train"])
+    val_df = pd.read_csv(dataset_paths["val"])
+    test_df = pd.read_csv(dataset_paths["test"])
+
+    le = LabelEncoder()
+    train_df.rate = le.fit_transform(train_df.label)
+
+    train = Dataset.from_pandas(train_df)
+    val = Dataset.from_pandas(val_df)
+    test = Dataset.from_pandas(test_df)
+
+    return train, val, test, le
 
 
 if __name__ == "__main__":
